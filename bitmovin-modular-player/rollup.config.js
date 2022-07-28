0b7735b5d1506e1baa52efbff920818091283502
biswasindhu.mandal <exambiswa10@gmail.com>
2022-07-28 10:38:09 +0530

    rolljs css plugins

--- bitmovin-modular-player/rollup.config.js
+++ bitmovin-modular-player/rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs'
+import css from "rollup-plugin-import-css";
 
 export default {
     input: "src/AmcBitmovin.js",
@@ -11,6 +12,7 @@ export default {
     },
     plugins: [
         resolve(),
-        commonjs()
+        commonjs(),
+        css()
     ]
 }
