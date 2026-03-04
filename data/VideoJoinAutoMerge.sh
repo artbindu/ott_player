a03a29bdf37ec9b6e4254358448171b099c7b0cb
Biswasindhu Mandal <sindhu.online.24x7@gmail.com>
2026-03-04 23:55:03 +0530

    update video

--- data/VideoJoinAutoMerge.sh
+++ data/VideoJoinAutoMerge.sh
@@ -4,15 +4,19 @@
 DIR="trim"
 
 # Output file
-OUTPUT_FILE="mergevideolist.txt"
+OUTPUT_FILE="videolist.txt"
 
 # Clear the output file
 > "$OUTPUT_FILE"
 
-# List all files, sort by creation/modification time (oldest first), and write to input.txt
-find "$DIR" -maxdepth 1 -type f -printf "%T@ %p\n" | sort -n | cut -d' ' -f2- | while read -r file; do
+# List all files, sort by filename ascending, and write to videolist.txt
+find "$DIR" -maxdepth 1 -type f | sort | while read -r file; do
   echo "file $file" >> "$OUTPUT_FILE"
 done
 
-# Auto Merge Videos
-ffmpeg -f concat -safe 0 -i mergevideolist.txt -c copy mergedCropVideo.mp4
+# Merge videos using ffmpeg
+ffmpeg -f concat -safe 0 -i "$OUTPUT_FILE" -c copy mergedCropVideo.mp4
+# # Remove Audio From Video
+ffmpeg -i "mergedCropVideo.mp4" -ss 1.1 -t 350.0 -c:v libx264 -crf 23 -preset fast -an "mergedCropVideo_mute.mp4"
+# Merged Audio Video
+ffmpeg -i mergedCropVideo_mute.mp4 -i finalAudio.mp3 -c:v copy -c:a aac -shortest output.mp4
